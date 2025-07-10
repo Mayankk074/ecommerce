ccc8cdb2cb9c16aa3a7baa8372b5612b34ebcd58
Mayankk074 <mayankk074@gmail.com>
2025-07-10 10:47:43 +0530

    Sending Map from Usercontroller

--- backend/ecom/src/main/java/com/foryou/ecom/controller/UserController.java
+++ backend/ecom/src/main/java/com/foryou/ecom/controller/UserController.java
@@ -3,9 +3,15 @@ package com.foryou.ecom.controller;
 import com.foryou.ecom.model.Users;
 import com.foryou.ecom.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -14,12 +20,27 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/register")
-    public Users register(@RequestBody Users user){
-        return service.register(user);
+    public ResponseEntity<Map<String, Users>> register(@RequestBody Users user){
+        Users user1=service.register(user);
+        if(user1 != null){
+            Map<String, Users> response=new HashMap<>();
+            response.put("user", user1);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody Users user){
-        return service.verify(user);
+    public ResponseEntity<Map<String, Object>> login(@RequestBody Users user){
+        String token=service.verify(user);
+        if(token != null){
+            Map<String, Object> response = new HashMap<>();
+            response.put("token", token);
+            response.put("user", user);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
--- backend/ecom/src/main/java/com/foryou/ecom/model/Users.java
+++ backend/ecom/src/main/java/com/foryou/ecom/model/Users.java
@@ -1,12 +1,16 @@
 package com.foryou.ecom.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Users {
 
+    //generating identity automaticaly in postgresql
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
